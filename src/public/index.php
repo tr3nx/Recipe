555e1b13719fc9c752c5b29635e394903eef50d8
tr3nx <0wnedbytr3nx@gmail.com>
2019-09-22 18:13:43 -0700

    Modify database/model, remove facade idea, clean up code, remove logging & cache

--- src/public/index.php
+++ src/public/index.php
@@ -2,7 +2,7 @@
 
 define('APP_START', microtime(true));
 
-require __DIR__ . "/../vendor/autoload.php";
+require __DIR__ . '/../vendor/autoload.php';
 
 $app = \Core\App::getInstance();
 $app->boot();
