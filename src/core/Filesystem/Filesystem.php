86c8ffe920aa99b245b68d61b8b5cd57a4bccb10
tr3nx <0wnedbytr3nx@gmail.com>
2019-10-24 17:57:31 -0700

    Filesystem functions

---
+++ src/core/Filesystem/Filesystem.php
@@ -0,0 +1,100 @@
+<?php
+
+class Filesystem {
+	public static function exists($path) {
+		return file_exists($path);
+	}
+
+	public static function isFile($path) {
+		return is_file($path);
+	}
+
+	public static function isDir($path) {
+		return is_dir($path);
+	}
+
+	public static function isWritable($path) {
+		return is_writable($path);
+	}
+
+	public static function isReadable($path) {
+		return is_readable($path);
+	}
+
+	public static function type($path) {
+		return filetype($path);
+	}
+
+	public static function size($path) {
+		return filesize($path);
+	}
+
+	public static function ext($path) {
+		return pathinfo($path, PATHINFO_EXTENSION);
+	}
+
+	public static function mime($path) {
+		return finfo_file(finfo_open(FILEINFO_MIME_TYPE), $path);
+	}
+
+	public static function basename($path) {
+		return pathinfo($path, PATHINFO_BASENAME);
+	}
+
+	public static function filename($path) {
+		return pathinfo($path, PATHINFO_FILENAME);
+	}
+
+	public static function lastModified($path) {
+		return filemtime($path);
+	}
+
+	public static function hash($path) {
+		return md5_file($path);
+	}
+
+	public static function move($src, $dst) {
+		return rename($src, $dst);
+	}
+
+	public static function copy($src, $dst) {
+		return copy($src, $dst);
+	}
+
+	public static function delete($path) {
+		try {
+			if (@unlink($path)) {
+				return true;
+			}
+		} catch (ErrorException $e) {
+			return false;
+		}
+		return false;
+	}
+
+	public static function chmod($path, $mode = null) {
+		if ($mode) {
+			return chmod($path, $mode);
+		}
+		return substr(sprintf('%o', fileperms($path)), -4);
+	}
+
+	public static function get($path) {
+		return file_get_contents($path);
+	}
+
+	public static function put($path, $contents) {
+		return file_put_contents($path, $contents);
+	}
+
+	public static function append($path, $data) {
+		return file_put_contents($path, $data, FILE_APPEND);
+	}
+
+	public static function prepend($path, $data) {
+		if ($this->exists($path)) {
+			return $this->put($path, $data . $this->get($path));
+		}
+		return $this->put($path, $data);
+	}
+}
